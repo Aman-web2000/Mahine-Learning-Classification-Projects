495a30dd38e6f616c321ea6765cb4a52f7740849
Aman Chauhan <66059307+Aman-web2000@users.noreply.github.com>
2022-02-20 12:43:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,154 +1,92 @@
-from ast import increment_lineno
 import streamlit as st 
 import numpy as np
 import pandas as pd
-import seaborn as sns 
-import matplotlib.pyplot as plt
 import pickle
 from PIL import Image
-import os.path
-from pathlib import Path
-
-plt.style.use('fivethirtyeight')
-
-
-
-## Creating Pages 
-next = st.sidebar.button('Next on list')
-
-new_choice=["About",'Vizualization','Insights','Prediction']
-
-if os.path.isfile('next.p'):
-    next_clicked = pickle.load(open('next.p', 'rb'))
-    
-    if next_clicked == len(new_choice):
-        next_clicked = 0 
-else:
-    next_clicked = 0
-
-if next:
-    next_clicked = next_clicked +1
-    if next_clicked == len(new_choice):
-        next_clicked = 0 
-
-choice = st.sidebar.radio("go to",("About",'Vizualization','Insights','Prediction'), index=next_clicked)
-
-pickle.dump(new_choice.index(choice), open('next.p', 'wb'))
-
-## Importing Images
-image='Stöwer_Titanic.jpg'
-
 
 pickle_in=open('Titanic.pkl','rb')
 clf=pickle.load(pickle_in)
 
-## Importing Data Set
-path='new_df.csv'
-df=pd.read_csv(path)
-cat_cols=[col for col in df.columns if df[col].dtype=='O']
-num_cols=[col for col in df.columns if df[col].dtype!='O']
-
-## Prediciton function
 def predictor(l):
     prediction=clf.predict([l])
     return prediction[0]
 
-## For Data Vizualization
-def count_plot(i):
-    fig = plt.figure(figsize=(10, 4))
-    sns.countplot(x =i, data = df)
-    st.pyplot(fig)
-
-def hist_plot(i):
-    fig = plt.figure(figsize=(10, 4))
-    sns.histplot(x =i, data = df)
-    st.pyplot(fig)
-
-def box_plot(i,j):
-    fig = plt.figure(figsize=(10, 4))
-    sns.boxplot(x =i,y=j, data = df)
-    st.pyplot(fig)
-
-def line_plot(i):
-    fig = plt.figure(figsize=(10, 4))
-    sns.lineplot(x =i, data = df)
-    st.pyplot(fig)
-
-def scatter_plot(i,j):
-    fig = plt.figure(figsize=(10, 4))
-    sns.scatterplot(x =i,y=j, data = df)
-    st.pyplot(fig)
+## Background Image
+st.markdown(
+         f"""
+         <style>
+         .stApp {{
+             background: url("https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F28%2F2016%2F09%2FRussian-Wreck-Zabagad-SHIPS0816-2000.jpg");
+             background-size: cover
+         }}
+         </style>
+         """,
+         unsafe_allow_html=True
+     )
 
 
-## WEb page
+# Side bar Design 
+
+html_sidebar_heading="""
+<div style='background-color:#0b2038;'>
+<h1 style="color: white; text-align: center">Prediction</h1>
+</div>
+"""
+
+st.sidebar.markdown(html_sidebar_heading,unsafe_allow_html=True)
+
+st.sidebar.subheader('Enter the Data Below :')
+
+Pclass= st.sidebar.selectbox("Passenger Class",['1','2','3'])
+Age= st.sidebar.slider("Age",min_value=2,max_value=80,step=2)
+fare= st.sidebar.slider("Fare",min_value=10,max_value=1000,step=40)
+no_of_family_members= st.sidebar.slider("No of Fmaily members on ship",min_value=0,max_value=20,step=1)
+sex= st.sidebar.radio('Sex',['Male','Female'])
+Married= st.sidebar.radio("Married",['Yes',"No"])
+Embarked= st.sidebar.selectbox("Embarked",['S','C','Q'])
+
+l=[Pclass,Age,fare,no_of_family_members,sex,Married,Embarked]
+
+# Main Page 
+
 def main():
-    
-    if choice=='About': 
-        st.title("Titanic Survival Model")
-        st.image(image)
-        st.write(''' 
-        The sinking of the Titanic is one of the most infamous shipwrecks in history.
 
-On April 15, 1912, during her maiden voyage, the widely considered “unsinkable” RMS 
-Titanic sank after colliding with an iceberg. Unfortunately, 
-there weren’t enough lifeboats for everyone onboard, 
-resulting in the death of 1502 out of 2224 passengers and crew.
-
-While there was some element of luck involved in surviving, it seems some 
-groups of people were more likely to survive than others.
-
-This Model predicts whether you would have survived this tragedy or not based on the data that you 
-enter in the model .
-
-Good Luck!
-''')
-        
-    if choice=='Insights':
-        st.title("Insights drawn from the Data")
-        insights_path="Insights.html"
-        file=open(insights_path)
-        line=file.readlines()
-        for i in line:
-            print(st.markdown(i),end=' ')    
-    
-    elif choice=='Vizualization':
-
-        st.title('Vizualizing Data')
-
-        graph=st.selectbox('Plot',['Countplot','Boxplot','Lineplot','Histogram','Scatterplot'])
-        
-        i=st.selectbox('Categorical Features',cat_cols)
-        j=st.selectbox('Numerical features',num_cols)
-
-        if graph=='Countplot':
-            count_plot(i)
-        elif graph=='Boxplot':
-            box_plot(i,j)
-        elif graph=='Histogram':
-            hist_plot(i)
-        elif graph=='Scatterplot':
-            scatter_plot(i,j)
-
-        
-    
-    elif choice=='Prediction':
-        html_temp = """
-    <div style="background-color:tomato;padding:10px">
-    <h2 style="color:white;text-align:center;"> Prediction </h2>
+    html_page_title="""
+    <div style="background-color: red;">
+    <h1 style="text-align: center; color: white;">Titanic Survival Prediction</h1>
     </div>
     """
-        st.markdown(html_temp,unsafe_allow_html=True)
-        st.subheader('Enter the Data Below :')
-        Pclass= st.selectbox("Passenger Class",['1','2','3'])
-        Age= st.slider("Age",min_value=2,max_value=80,step=2)
-        fare= st.slider("Fare",min_value=10,max_value=1000,step=40)
-        no_of_family_members= st.slider("No of Fmaily members on ship",min_value=0,max_value=20,step=1)
-        sex= st.radio('Sex',['Male','Female'])
-        Married= st.radio("Married",['Yes',"No"])
-        Embarked= st.selectbox("Embarked",['S','C','Q'])
-        l=[Pclass,Age,fare,no_of_family_members,sex,Married,Embarked]
 
-        if st.button("Predict"):
+    st.markdown(html_page_title,unsafe_allow_html=True)
+
+    image_path='https://digitaldomain.com/wp-content/uploads/2019/09/Titanic_2.jpg'
+    st.image(image_path)
+    
+    html_about='''<div style="background-color: black; padding: 10px;">
+    <p style="text-align: center; color: aliceblue;">The sinking of the Titanic is one of the most infamous shipwrecks in history.
+    <br>
+    <br>
+    On April 15, 1912, during her maiden voyage, the widely considered “unsinkable” RMS 
+    Titanic sank after colliding with an iceberg. Unfortunately, 
+    there weren’t enough lifeboats for everyone onboard, 
+    resulting in the death of 1502 out of 2224 passengers and crew.
+    <br>
+    <br>
+    While there was some element of luck involved in surviving, it seems some 
+    groups of people were more likely to survive than others.
+    <br>
+    <br>
+    This Model predicts whether you would have survived this tragedy or not based on the data that you 
+    enter in the model .
+    <br>
+    <br>
+    Good Luck!</p>
+    </div>'''
+
+    st.markdown(html_about,unsafe_allow_html=True)
+
+
+    if st.sidebar.button("Predict"):
             if l[0]=='1':
                 l[0]=3
             elif l[0]=='3':
@@ -181,15 +119,21 @@ Good Luck!
                 l.append(1)
             
             ans=predictor(l)
-            st.write(l)
-            st.write(ans)
+            
             if ans==1:
-               st.subheader("Hurrah! You would have Survived the Titanic")
+                survive="""
+                <div style="background-color: #4508EF;padding: 10px;">
+                <h1 style="text-align: center; color: white;">Hurrah! 👏 You have Survived the Titanic</h1>
+                </div>
+                """
+                st.markdown(survive,unsafe_allow_html=True)
             else:
-                st.subheader("R.I.P")
+                not_survived="""
+                <div style="background-color: #900C3F; padding: 10px ">
+                <h1 style="color: White; text-align: center;">R.I.P ☠️</h1>
+                </div>
+                """
+                st.markdown(not_survived,unsafe_allow_html=True)
 
-
-
-
-if __name__== "__main__":
+if __name__=="__main__":
     main()
